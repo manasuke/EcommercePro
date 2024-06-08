8fa762deefeeeecc1a4d106e53cc4ff0b9f45748
manasuke <ngocthai0789@gmail.com>
2024-06-08 20:24:35 +0700

    #3 How to Integrate Html Template
    Lỗi dropdown User menu, thay bằng logout button

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,9 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
